168c84de8bbc353a71253da7c09a55a3601a744b
ketangote <ketan.gote@metamagic.in>
2019-09-03 21:19:26 +0530

    Changes: Updated POM

--- src/main/java/com/mm/service/MockUiServiceImpl.java
+++ src/main/java/com/mm/service/MockUiServiceImpl.java
@@ -19,10 +19,12 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.context.annotation.RequestScope;
 
+import atg.taglib.json.util.JSONException;
+import atg.taglib.json.util.JSONObject;
+
 @Service
 @RequestScope
 public class MockUiServiceImpl implements MockUiService {
-
 
 	@Value("${litty.url}")
 	private String littyUrl;
@@ -30,12 +32,9 @@ public class MockUiServiceImpl implements MockUiService {
 	@Value("${request.location}")
 	private String fileLocation;
 
-	
-	
-	@Autowired 
+	@Autowired
 	private RestTemplate restTemplate;
-	
-	
+
 	@Override
 	public void sendMockCommandRequest(String command) {
 		this.sendMockCommandRequest(command, 1);
@@ -43,59 +42,85 @@ public class MockUiServiceImpl implements MockUiService {
 
 	@Override
 	public void sendMockCommandRequest(String command, Integer nooftimes) {
-		System.out.println("Command Received "+ command+" to execute, will be executed "+nooftimes+" times");
-		
-		for(int i=0; i<nooftimes; i++){
-			String strRequest = this.getRequest(command);
-			System.out.println("Sending request with body as "+ strRequest);
-			try {
-				this.sendRequest(strRequest);
-			} catch (UnsupportedEncodingException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
+		System.out.println("Command Received " + command + " to execute, will be executed " + nooftimes + " times");
+
+		for (int i = 0; i < nooftimes; i++) {
+			this.sendRequest(command, 1);
 		}
-		
 
 	}
-	
-	private void sendRequest(String requestPayload) throws UnsupportedEncodingException{
-		
-		if(littyUrl == null){
+
+	private void sendRequest(String command, Integer seq) {
+
+		String strRequest = this.getRequest(command + "_" + 1);
+		System.out.println("Sending request with body as " + strRequest);
+		try {
+			this.sendRequest(strRequest);
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	private void sendRequest(String requestPayload) throws UnsupportedEncodingException {
+
+		if (littyUrl == null) {
 			System.out.println("Litty url is no defined {} ");
 		}
 		MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
-		//map.add("initial","true");
-		//map.add("data",requestPayload);
-		
-		String finalUrl = littyUrl +"?initial=true&data="+URLEncoder.encode(requestPayload, "UTF-8");
-		System.out.println("FINAL URL "+finalUrl);
+		// map.add("initial","true");
+		// map.add("data",requestPayload);
+
+		String finalUrl = littyUrl + "?initial=true&data=" + URLEncoder.encode(requestPayload, "UTF-8");
+		System.out.println("FINAL URL " + finalUrl);
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-		
-		HttpEntity<MultiValueMap<String, String>> entity =
-			    new HttpEntity<MultiValueMap<String, String>>(map, headers);
-		
+
+		HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity<MultiValueMap<String, String>>(map, headers);
+
 		ResponseEntity<Object> response = restTemplate.exchange(finalUrl, HttpMethod.POST, entity, Object.class);
-		
-		System.out.println("Response body "+response.getBody());
-		
+		System.out.println("Response body " + response.getBody());
+		System.out.println("CHECKING for multipleRequest");
+		this.multipleRequest(response.getBody().toString());
 	}
-	
-	
+
+	private void multipleRequest(String body) {
+
+		try {
+			JSONObject json = new JSONObject(body);
+			if (json.has("Data") && json.getJSONObject("Data").has("ResponseMetaData")) {
+				JSONObject responseMetaData = json.getJSONObject("Data").getJSONObject("ResponseMetaData");
+				String completed = responseMetaData.getString("completed");
+				if (completed != null && completed.equals("false")) {
+					if (responseMetaData.has("commandSeqId") && responseMetaData.has("totalResponsesAvailable")
+							&& responseMetaData.has("Command")) {
+						Integer commandSeqId = Integer.valueOf(responseMetaData.getString("commandSeqId"));
+						String command = responseMetaData.getString("Command");
+
+						commandSeqId++;
+						this.sendRequest(command, commandSeqId);
+					}
+				}
+			}
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+
+	}
 
 	private String getRequest(String command) {
 		StringBuilder sb = new StringBuilder();
 		String line;
 
 		if (fileLocation == null) {
-			System.out.println("File lcoation is not defined "+ fileLocation);
+			System.out.println("File lcoation is not defined " + fileLocation);
 		}
 
 		try {
 
-			BufferedReader br = new BufferedReader(new FileReader(new File(fileLocation+File.separator+command+".json")));
+			BufferedReader br = new BufferedReader(
+					new FileReader(new File(fileLocation + File.separator + command + ".json")));
 
 			while ((line = br.readLine()) != null) {
 				sb.append(line);
